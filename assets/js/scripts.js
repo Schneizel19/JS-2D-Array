5c0e729afcb154700f43273aff907e6978debf9f
Jake Laylo <danilo_jake16@yahoo.com>
2023-08-04 15:24:13 +0800

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,133 @@
+//array of items
+// index
+
+// var aItems = [
+//     'Can Goods', 
+//     'Rice', 
+//     'Junkfoods', 
+//     'Liqour', 
+//     'Pork',
+// ];
+
+var aItems = [
+    /* 0 => */  [
+        /* 0 => */'Can Goods', 
+        /* 1 => */360
+    ],
+
+    ['Rice', 1300],
+    ['Junkfoods', 124],
+    ['Liquor', 275],
+    ['Pork', 330],   
+];
+
+displayItems(aItems);
+
+function displayItems(arrItems) {
+    var sList = "";
+    for(var i = 0; i < arrItems.length; i++) {
+        // sList += "<li><span>" + i + " : " + arrItems[i] + "</span><button onclick='removeItem("+ i +");'>X</button></li>";
+        sList += `<li>${i}:<span>${arrItems[i][0]}</span><span>${arrItems[i][1]}</span>
+            <button onclick='editItem(${i});'>E</button>
+            <button onclick='removeItem(${i});'>X</button>
+        </li>`;
+    }
+    
+    document.querySelector("#ul-lists").innerHTML = sList;
+    console.table(aItems);
+}
+
+
+
+//Edit Item on List
+let editValue = '';
+let editPrice = '';
+let indexHolder = 0;
+
+function editItem(arrIndex) {
+    indexHolder = arrIndex;
+    editValue = aItems[arrIndex][0]
+    editPrice = aItems[arrIndex][1];
+
+    const newValue = document.querySelector('#txtItem').value = editValue;
+    const newPrice = document.querySelector('#txtPrice').value = editPrice;
+
+    displayItems(aItems);
+}
+
+// Update Item on List
+// document.querySelector('#btnUpdate').addEventListener('click', function(arrNum) {
+//     arrNum = indexHolder;
+//     editValue = document.querySelector('#ul-lists').innerText = aItems[arrNum][0];
+//     editPrice = document.querySelector('#ul-lists').innerText = aItems[arrNum][1];
+
+//     console.log(editValue);
+//     console.log(editPrice);
+    
+//     const newValue = document.querySelector('#txtItem').value = editValue;
+//     const newPrice = document.querySelector('#txtPrice').value = editPrice;   
+    
+    // aItems[arrNum] = [editValue, editPrice];
+//     aItems.splice(arrNum, 0, [newValue, newPrice]);
+//     console.log("index => " + arrNum);
+    
+//     displayItems(aItems);
+// });
+
+//Update the item on list
+document.querySelector('#btnUpdate').addEventListener('click', function() {
+    const newValue = document.querySelector('#txtItem').value;
+    const newPrice = Number(document.querySelector('#txtPrice').value);
+
+    if (!isNaN(newPrice)) {
+        aItems[indexHolder] = [newValue, newPrice];
+        displayItems(aItems);
+        clearInputFields();
+    }
+});
+
+//Remove Item on List
+function removeItem(arrIndex) {
+    aItems.splice(arrIndex, 1);
+    displayItems(aItems);
+}
+
+//Adding Item on Top of List
+document.querySelector('#btnAddTop').addEventListener('click', function() {
+    const addTopValue = document.querySelector('#txtItem').value;
+    const addTopPrice = Number(document.querySelector('#txtPrice').value);
+
+    aItems.unshift([addTopValue, addTopPrice]);
+
+    clearInputFields();
+    displayItems(aItems);
+});
+
+//Add Item on Bottom of List
+document.querySelector('#btnAddBot').addEventListener('click', function() {
+    const addBotValue = document.querySelector('#txtItem').value;
+    const addBotPrice = Number(document.querySelector('#txtPrice').value);
+
+    aItems.push([addBotValue, addBotPrice]);
+
+    clearInputFields();
+    displayItems(aItems);
+});
+
+//Remove Top item 
+document.querySelector('#btnDelTop').addEventListener('click', function() {
+    aItems.shift();
+    displayItems(aItems);
+});
+
+//Remove Bottom item
+document.querySelector('#btnDelBot').addEventListener('click', function() {
+    aItems.pop();
+    displayItems(aItems);
+});
+
+// Clear the entered fields
+function clearInputFields() {
+    document.querySelector('#txtItem').value = '';
+    document.querySelector('#txtPrice').value = '';
+}
